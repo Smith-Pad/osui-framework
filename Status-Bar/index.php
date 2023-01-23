cea48828193d28579358ef1473d6a7471aff7c5c
senalbulumulle <senal.bulumulle@gmail.com>
2023-01-23 11:52:15 -0500

    Added Status-Bar [Not Functional Yet...]

---
+++ Status-Bar/index.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="index.css" type="text/css">
+
+
+<div class="status-bar">
+        <div class="left-grid">
+
+        </div>
+        <div class="center">
+
+        </div>
+        <div class="right-grid">
+
+        </div>
+</div>
